f92f6a22c19d70529fa07c08773f314120a2c71b
Softiko <Softiko@LAPTOP-9GLPJA1.mshome.net>
2018-12-24 22:48:15 +0100

    add admin panel and showing list of users

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,2 +1,34 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<h1>Users</h1>
+
+<table border="2">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>NAME</th>
+            <th>EMAIL</th>
+            <th>CREATED</th>
+            <th>UPDATED</th>
+        </tr>
+    </thead>
+    <tbody>
+        @if($users)
+        
+            @foreach($users as $user)
+                <tr>
+                    <td>{{$user->id}}</td>
+                    <td>{{$user->name}}</td>
+                    <td>{{$user->email}}</td>
+                    <td>{{$user->created_at->diffForHumans()}}</td>
+                    <td>{{$user->updated_at->diffForHumans()}}</td>
+                </tr>
+            @endforeach
+        @endif
+    </tbody>
+</table>
 
 
+@endsection

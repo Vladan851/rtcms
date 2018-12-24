daa4f2a1c200ed826f456fad970dcd9ceb3450ea
Vladan <vladan.mastilovic@gmail.com>
2018-12-24 16:05:01 +0100

    Small changes relations and testing.

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'role_id', 'email', 'password',
     ];
 
     /**
@@ -27,4 +27,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    
+    public function role() {
+        return $this->belongsTo('App\Role');
+    }
+   
 }
